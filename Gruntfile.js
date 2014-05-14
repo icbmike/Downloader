e029a097a87326951431fa762ce2650f4640046c
Michael Little <bio595@gmail.com>
2014-05-14 21:21:08 +1200

    Added grunt jshint plugin

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,13 @@
 module.exports = function(grunt) {
   // Do grunt-related things in here
 
-  grunt.grunt.initConfig({
-  	
+  grunt.initConfig({
+  	jshint: {
+  		src: ['Gruntfile.js', 'downloader/static/scripts/*.js']
+  	}
   });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  grunt.registerTask('default', 'jshint');
 };
